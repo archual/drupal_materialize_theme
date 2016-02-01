f9df042b390e77bdb453690180d45a740a2292cd
archual <archual@coldbrainweb.com>
2016-02-01 18:52:23 +0600

    Add theme-settings.php file, for theme settings form.

---
+++ theme-settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * theme-settings.php
+ *
+ * Provides theme settings for Materialize based themes.
+ *
+ * @see ./includes/settings.inc
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
