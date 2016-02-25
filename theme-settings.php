8e257f72d7743f4cdcd66930618412fa25165ff5
archual <archual@coldbrainweb.com>
2016-02-25 18:54:49 +0600

    Add theme-settings for navigation and breadcrumbs.

--- theme-settings.php
+++ theme-settings.php
@@ -24,16 +24,12 @@ function materialize_form_system_theme_settings_alter(&$form, FormStateInterface
   // Do not add Bootstrap specific settings to non-bootstrap based themes.
   $args = $form_state->getBuildInfo()['args'];
   $theme = !empty($args[0]) ? $args[0] : FALSE;
-//  if (isset($form_id) || $theme === FALSE || !in_array('bootstrap', _bootstrap_get_base_themes($theme, TRUE))) {
   if (isset($form_id) || $theme === FALSE) {
     return;
   }
 
   $form['materialize'] = array(
     '#type' => 'vertical_tabs',
-//    '#attached' => array(
-//      'library'  => array('bootstrap/adminscript'),
-//    ),
     '#prefix' => '<h2><small>' . t('Materialize Settings') . '</small></h2>',
     '#weight' => -10,
   );
@@ -45,18 +41,64 @@ function materialize_form_system_theme_settings_alter(&$form, FormStateInterface
     '#group' => 'materialize',
   );
 
-  // @TODO Add prefix for theme settings.
-//  function bootstrap_setting($name, $theme = NULL, $prefix = 'bootstrap') {
-//    $prefix = !empty($prefix) ? $prefix . '_' : '';
-//    $setting = theme_get_setting($prefix . $name, $theme);
-//    return $setting;
-//  }
+  // Components.
+  $form['components'] = array(
+    '#type' => 'details',
+    '#title' => t('Components'),
+    '#group' => 'materialize',
+  );
 
-  $form['general']['navbar_position'] = array(
+  // Breadcrumbs.
+  $form['components']['breadcrumb'] = array(
+    '#type' => 'details',
+    '#title' => t('Breadcrumbs'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+  $form['components']['breadcrumb']['breadcrumbs'] = array(
+    '#type' => 'select',
+    '#title' => t('Breadcrumbs visibility'),
+    '#default_value' => theme_get_setting('breadcrumbs', $theme),
+    '#options' => array(
+      0 => t('Hidden'),
+      1 => t('Visible'),
+    ),
+  );
+  $form['components']['breadcrumb']['breadcrumbs_home'] = array(
     '#type' => 'checkbox',
+    '#title' => t('Show "Home" breadcrumbs link'),
+    '#default_value' => theme_get_setting('breadcrumbs_home', $theme),
+    '#description' => t('If your site has a module dedicated to handling breadcrumbs already, ensure this setting is enabled.'),
+    '#states' => array(
+      'invisible' => array(
+        ':input[name="breadcrumbs"]' => array('value' => 0),
+      ),
+    ),
+  );
+  $form['components']['breadcrumb']['breadcrumbs_title'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show current page title at end'),
+    '#default_value' => theme_get_setting('breadcrumbs_title', $theme),
+    '#description' => t('If your site has a module dedicated to handling breadcrumbs already, ensure this setting is disabled.'),
+    '#states' => array(
+      'invisible' => array(
+        ':input[name="breadcrumbs"]' => array('value' => 0),
+      ),
+    ),
+  );
+
+  // Navbar.
+  $form['components']['navbar_position'] = array(
+    '#type' => 'details',
     '#title' => t('Navbar position'),
-    '#default_value' => theme_get_setting('navbar_position', $theme),
-    '#description' => t('Fixed header in top'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+
+  $form['components']['navbar_position']['fixed_header'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Fixed header in top'),
+    '#default_value' => theme_get_setting('fixed_header', $theme),
   );
 
   // Layouts.
